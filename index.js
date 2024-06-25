44734f3aff6c820f65b0f4dda2d7fe32390477e9
Paulo-galvao <paulogalvaoj21@gmail.com>
2024-06-25 19:04:25 -0300

    implementação do mdc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let numeroA = +0;
 let numeroB = +0;
 let resto = +1;
+let mdc = 1;
 
 const readline = require('readline')
 
@@ -42,7 +43,46 @@ rl.question("Digite o primeiro número: ", (primeiroNumero) => {
             var resultado = calcularMMC(numeroA, numeroB);
 
             console.log("O minimo múltiplo comum entre os dois números digitados é: "+resultado);
+            
+            // calculo do mdc
+
+            console.log("Digite dois números e decubra qual o máximo divisor comum (MDC) entre eles: ");
+
+            rl.question("Digite o primeiro número: ", function(numero1) {
+              parseFloat(numero1);
+              rl.question("Digite o segundo número: ", function(numero2) {
+                parseFloat(numero2);
+                
+                let i
+        let maior
+        let menor
+
+        if(numero1 > numero2) {
+            maior = numero1
+            menor = numero2
+        } else {
+            maior = numero2
+            menor = numero1
+        }
+
+        i = maior
+        
+
+        i = menor
+
+        for(i; i > 0; i--) {
+            if(numero1 % i == 0 && numero2 % i ==0) {
+                console.log("MDC: " + i)
+                break;
+            }
+        }
+
+                rl.close();
+              })
+            })
 
         })
 
 })
+
+
